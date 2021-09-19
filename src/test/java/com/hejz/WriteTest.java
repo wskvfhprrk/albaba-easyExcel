75bc7d82e6f6a180906ab7b39df81528867c7672
hejz <75412985@qq.com>
2021-09-19 22:26:36 +0800

    初始化——添加simpleWrite的xls和csv文件测试

--- src/test/java/com/hejz/WriteTest.java
+++ src/test/java/com/hejz/WriteTest.java
@@ -1,6 +1,7 @@
 package com.hejz;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.support.ExcelTypeEnum;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,15 +9,54 @@ import java.util.Date;
 import java.util.List;
 
 public class WriteTest {
+    /**
+     * 写xlsx
+     */
     @Test
-    public void writeTest() {
+    public void simpleWriteXlsx() {
         // 写法1 JDK8+
         // since: 3.0.0-beta1
         String fileName = "/Users/hejianzhe/Desktop/simpleWrite.xlsx";
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         // 如果这里想使用03 则 传入excelType参数即可
         EasyExcel.write(fileName, PersonExcelDto.class)
-                .sheet("模板")
+                .sheet("模板") //sheet标签名称
+                .doWrite(() -> {
+                    // 分页查询数据
+                    return data();
+                });
+
+    }
+
+    /**
+     * 写xls——占空间比较大，最多一相标签写65565条
+     */
+    @Test
+    public void simpleWriteXls() {
+        // 写法1 JDK8+
+        // since: 3.0.0-beta1
+        String fileName = "/Users/hejianzhe/Desktop/simpleWrite.xls";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, PersonExcelDto.class).excelType(ExcelTypeEnum.XLS)
+                .sheet("模板") //sheet标签名称
+                .doWrite(() -> {
+                    // 分页查询数据
+                    return data();
+                });
+
+    }/**
+     * 写csv
+     */
+    @Test
+    public void simpleWriteCsv() {
+        // 写法1 JDK8+
+        // since: 3.0.0-beta1
+        String fileName = "/Users/hejianzhe/Desktop/simpleWrite.csv";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, PersonExcelDto.class).excelType(ExcelTypeEnum.CSV)
+                .sheet("模板") //sheet标签名称
                 .doWrite(() -> {
                     // 分页查询数据
                     return data();
