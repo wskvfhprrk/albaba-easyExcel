f2d0cc593e77269e266b466aaf7785c928656fae
hejz <75412985@qq.com>
2021-09-19 22:28:20 +0800

    添加标题注解

--- src/main/java/com/hejz/PersonExcelDto.java
+++ src/main/java/com/hejz/PersonExcelDto.java
@@ -1,12 +1,17 @@
 package com.hejz;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class PersonExcelDto {
+    //@ExcelProperty标题注解
+    @ExcelProperty("姓名")
     private String name;
+    @ExcelProperty("年龄")
     private Integer age;
+    @ExcelProperty("生日")
     private Date birthday;
 }
